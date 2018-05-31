5c13199689f55efd5223eb7ed1fbf64dbe039fa8
todd forester <toddforester1989@gmail.com>
2018-05-31 00:12:27 -0500

    Im Done?
    if you find bugs post them here or on GBATemp. im pretty sure im done with this im tired of looking at it, it hurts mine eyes. next...

--- src/main.hpp
+++ src/main.hpp
@@ -10,7 +10,7 @@ const float MAX_ANGLE = 3.142 / 5.0;
 
 const float BALL_MAXSPEED = 8.0f;
 const float BALL_ACCELERATE = 1.05f;
-const float BALL_INIT_SPEED = 4.0f;
+const float BALL_INIT_SPEED = 4.5f;
 const int BALL_WIDTH = 25;
 const int BALL_HEIGHT = 25;
 
